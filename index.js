5b0da7b3d66cffff9547b67d80e606b1ac190cb4
tommaclean <tom.j.maclean@gmail.com>
2019-06-26 17:33:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-animal = ""
+const animal = "cat"
 
 function myAnimal() {
+  let animal = "dog"
   return animal
 }
 
@@ -12,8 +13,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
 
   // Feel free to move things around!
-  two = 2
+  let two = 2
+  return n + two
 }
